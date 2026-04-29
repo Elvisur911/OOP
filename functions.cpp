5e541d865b3a41f8b7474e0418c42e4cfb7b2c72
Elvisur911 <elvisstillflies@gmail.com>
2026-04-29 08:52:33 +0300

    more

---
+++ functions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int add(int a, int b){
+
+int r;
+r = a + b;
+return (r);
+}
+
+int main (){
+int z;
+z = add(56, 34);
+cout<<"Result :" << z;
+return 0;
+}
+
---
+++ funtions.cpp
@@ -0,0 +1,14 @@
+#include <iostream.h>
+
+int addition (int a , Int b){
+int r;
+r = a + b;
+return (r);
+}
+
+int main(){
+int z;
+z= addition (5,3);
+cout <<"result:" << z;
+return 0;
+}
